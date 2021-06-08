ab5edd7f8f8c09a3a8e112ff1e8b3f9a56b161ab
tene93393 <54301338+tene93393@users.noreply.github.com>
2021-06-08 15:32:36 +0700

    FrontEnd_BankAccount
    FrontEnd_BankAccount

---
+++ t&c.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Safe Online Banking</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Terms and conditions</u></h3>
+              
+            <div class="contact" style="overflow: scroll;">
+             
+           
